e4ab907ee3def102ea6da0310bd83ec3ec6f5539
Nithin-Mohan-p <nithinmohanp435@gmail.com>
2022-09-19 10:55:45 -0700

    first commit

---
+++ bin/sample.dart
@@ -0,0 +1,13 @@
+import 'package:test/expect.dart';
+
+void main() {
+  print("welcome to dart");
+  int a = 11;
+  int b = 23;
+  print("sum = ${a + b}");
+
+  String name = "Nithin";
+  print("my name is ${name}");
+  int age = 21;
+  print("i am ${age} years old");
+}
